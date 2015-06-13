c0600a045cb80632defd6f6ef24f5003ddb49563
Anthony Varghese <anthonyvar@gmail.com>
2015-06-12 21:36:35 -0500

    Lab 5 Set up

--- JavaFX/src/lab05a/Java05a1.java
+++ JavaFX/src/lab05a/Java05a1.java
@@ -1,7 +1,7 @@
 package lab05a;
 
 /**
- * Java05a - JavaFX program with colors for custom 3D shapes
+ * Java05a1 - JavaFX program with colors for custom 3D shapes
  *   
  * For more details see:
  * https://docs.oracle.com/javase/8/javafx/api/javafx/scene/shape/TriangleMesh.html
@@ -20,10 +20,15 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
@@ -31,9 +36,9 @@ import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class Java05a extends Application {
+public class Java05a1 extends Application {
 	
-	private final String TITLE = "Java05a: Colors for Custom 3D Mesh Shapes";
+	private final String TITLE = "Java05a1: Colors on 2D and 3D Shapes";
 	private final int SCENE_WIDTH  = 600;
 	private final int SCENE_HEIGHT = 300;
 
@@ -49,10 +54,11 @@ public class Java05a extends Application {
 			root.setAlignment( Pos.TOP_CENTER );
 			
 			Scene scene = new Scene(root, SCENE_WIDTH,SCENE_HEIGHT, enableDepthBuffer );
+			scene.setFill( Color.BLACK );
 			ObservableList<Node> list = root.getChildren();
 			
 			list.add( addPane() );
-			
+						
 			if (enableDepthBuffer){
 				PerspectiveCamera cam = new PerspectiveCamera();
 				// Position the camera to the left, up a little and back a bit
@@ -72,12 +78,9 @@ public class Java05a extends Application {
 	}
 
 	private Pane addPane(){
-		PhongMaterial rpm = new PhongMaterial();
-		rpm.setDiffuseColor(Color.RED);
-		PhongMaterial gpm = new PhongMaterial();
-		gpm.setDiffuseColor(Color.GREEN);
-		PhongMaterial bpm = new PhongMaterial();
-		bpm.setDiffuseColor(Color.BLUE);
+		PhongMaterial rpm = new PhongMaterial();	rpm.setDiffuseColor(Color.RED);
+		PhongMaterial gpm = new PhongMaterial();	gpm.setDiffuseColor(Color.GREEN);
+		PhongMaterial bpm = new PhongMaterial();	bpm.setDiffuseColor(Color.BLUE);
 
 		
 		Pane box = new Pane();
@@ -87,8 +90,7 @@ public class Java05a extends Application {
 		 * Nodes in this scene
 		 */
 		final Point3D CuboidP1 = new Point3D( 200, 120, 100);
-		MeshView cuboid = createRectangle( CuboidP1 );
-		cuboid.setMaterial( gpm );
+		Group cuboid = createRectangle( CuboidP1 );
 		
 		// Use 3-d boxes - long narrow ones - for each of the three axes.
 		Box xaxis = new Box(300,   2,   2);
@@ -109,44 +111,20 @@ public class Java05a extends Application {
 	    rAnimation.setCycleCount(Animation.INDEFINITE);
 	    rAnimation.play();
 
-		
 		list.add( things );
 		return box;
 	}
 	
 	
-	/**
-	 * convertToFloat
-	 *    create an array of floats from an array of doubles
-	 *    
-	 * @param in - double array
-	 * @return float array
-	 */
-	private float[] convertToFloat( double[] in ) {
-		float[] out = new float[ in.length ];
-
-		for (int i=0; i<in.length; i++)
-			out[i] = (float) in[i];
-		
-		return out;
-	}
-
 	/*
 	 * Set up a cuboid from 1 point in 3D space
-	 *  This method is for the simple case where the edges of the cuboid
-	 *  are parallel to the 3 axes - with this constraint one point is 
-	 *  the origin and a second point is all we need to specify the cuboid.
-	 *  Therefore, the x, y, and z components of the one point specify the
-	 *  width, height and depth of the cuboid.
-     *
-	 *  The more general case with two points in 3-d space to 
-	 *   specify a cuboid is harder because it means having to figure out
-	 *   the remaining 6 points of the cuboid - a lot of geometry
-	 *   in 3-dimensions
+	 *  The edges of the cuboid are parallel to the 3 axes - one point is 
+	 *  the origin and a second point specifies the cuboid. Therefore, the
+	 *  x, y, and z components are the width, height and depth of the cuboid.
 	 *   
 	 */
-	private MeshView createRectangle( Point3D p1 ) {
-		MeshView result = new MeshView();
+	private Group createRectangle( Point3D p1 ) {
+		MeshView meshview = new MeshView();
 		TriangleMesh mesh = new TriangleMesh(); 
 		/*
 		 * A TriangleMesh is a mesh of triangles. 
@@ -184,17 +162,12 @@ public class Java05a extends Application {
 		};
 		
 		/*
-		 * Need texture coordinates even if we don't have texture
+		 * Texture coordinates even if we don't have texture
 		 * Map the 8 vertices in the points array to the 
 		 * 2D unit square ( (0,0), (1,0), (1,1), (0,1) )
-		 * 
-		 * Here we decided to map the 6 rectangular faces of the cuboid to
-		 * the unit square using the following layout. This is just one
-		 * possibility - there are many more mappings that we can use.
-		 * 
-		 * 0,0 
+		 *  
 		 *    p2       p0           p7     p5
-		 *     .-------.------------.-------.
+		 * 0,0 .-------.------------.-------.
 		 *     |       |            |       |
 		 *     |     p6|          p3|       |p1
 		 *  p4 .-------.------------.-------.
@@ -206,8 +179,7 @@ public class Java05a extends Application {
 		 *     .     p2.------------.p5     .
 		 *     |       |            |       |
 		 *     |       |            |       |
-		 *     .-------.------------.-------. 1,1
-		 *             p0           p7
+		 *     .-----p0.----------p7.-------. 1,1
 		 */
 		float[] textures = {
 				0.0f, 0.0f,  //  t0 - p2
@@ -230,22 +202,18 @@ public class Java05a extends Application {
 		 * Create faces
 		 */
 		int[] faces = {
-				// front face triangles:
-				0,  1,   3,  6,   7,  2,
+				0,  1,   3,  6,   7,  2, // front face
 				0,  1,   6,  5,   3,  6,
-				// top face triangles:
-				2, 10,   7, 13,   5, 11,
+				2, 10,   7, 13,   5, 11, // top face 
 				2, 10,   0, 12,   7, 13,
-				// bottom face triangles:
-				6,  5,   1,  9,   3,  6,
+				6,  5,   1,  9,   3,  6, // bottom face
 				6,  5,   4,  8,   1,  9,
-				// back face triangles:
-				2, 10,   5, 11,   1,  9,
-				// left face triangles:
-				0,  1,   2,  0,   4,  4,
+				2, 10,   5, 11,   1,  9, // back face
+				2, 10,   1,  9,   4,  8,
+				0,  1,   2,  0,   4,  4, // left face
 				0,  1,   4,  4,   6,  5,
-				// right face triangles:
-				7,  2,   3,  6,   1,  7
+				7,  2,   3,  6,   1,  7, // right face
+				7,  2,   1,  7,   5,  3
 		};
 
 		/*
@@ -258,7 +226,32 @@ public class Java05a extends Application {
 		/*
 		 * mesh is done - set it in the MeshView object
 		 */
-		result.setMesh( mesh );
+		meshview.setMesh( mesh );
+		PhongMaterial mymatcolor = new PhongMaterial();
+		mymatcolor.setDiffuseColor( Color.RED );
+		meshview.setMaterial( mymatcolor );
+		meshview.setTranslateX( 20 );
+		meshview.setTranslateY( 20 );
+		meshview.setTranslateZ( 20 );
+		
+		
+		// Create a Canvas object to set up an ImagePattern
+		final int canvasW = 256;
+		final int canvasH = 256;
+		Canvas canvas = new Canvas( canvasW, canvasH );
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		LinearGradient lg = new LinearGradient(0, 0, canvasW, canvasH,
+                false, CycleMethod.NO_CYCLE,
+                new Stop(0, Color.RED  ),
+                new Stop(1, Color.BLUE ));
+		gc.setFill( lg );
+		gc.fillOval(0, 0, 256, 256);
+		canvas.setTranslateX( -100 );
+		canvas.setTranslateY( -100 );
+		canvas.setTranslateZ( 150 );
+
+		
+		Group result = new Group( meshview, canvas );
 		return result;
 	}
 
